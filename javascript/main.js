7d7a0e3f4eb7f4b7a725a7ea985bf0f3a4fa988e
Joaquinn <joaov.b.euwest@gmail.com>
2025-03-27 20:12:25 -0300

    #projeto-sanly

---
+++ javascript/main.js
@@ -0,0 +1,6 @@
+// scripts/main.js
+import { carregarProjetos } from "./projetos.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+    carregarProjetos();
+});
---
+++ javascript/projetos.js
@@ -0,0 +1,34 @@
+
+export function carregarProjetos() {
+
+  const projetos = [
+    { 
+        titulo: "Projeto 1", 
+        descricao: "Descrição do projeto 1",
+        imagem: "./imagens/imagem1.jpg"
+    },
+    { 
+        titulo: "Projeto 2", 
+        descricao: "Descrição do projeto 2",
+        imagem: "./imagens/imagem2.jpg"
+    },
+    { 
+        titulo: "Projeto 3", 
+        descricao: "Descrição do projeto 3",
+        imagem: "./imagens/imagem3.jpg"
+    }
+];
+
+const container = document.querySelector(".projeto-lista");
+
+projetos.forEach(projeto => {
+    const div = document.createElement("div");
+    div.classList.add("projeto");
+    div.innerHTML = `
+        <img src="${projeto.imagem}" alt="${projeto.titulo}" class="projeto-imagem">
+        <h3>${projeto.titulo}</h3>
+        <p>${projeto.descricao}</p>
+    `;
+    container.appendChild(div);
+  });
+}
