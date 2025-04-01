aebb5bf5985cfe5f90550acb80d30c3290a8be51
Joaquinn <joaov.b.euwest@gmail.com>
2025-04-01 18:15:49 -0300

    #aplicação da biblioteca aos

--- javascript/main.js
+++ javascript/main.js
@@ -1,12 +1,19 @@
 // scripts/main.js
-import { carregarProjetos } from "./projetos.js";
-import SlideNav from "./slide.js";
-
+import { carregarProjetos } from './projetos.js';
+import SlideNav from './slide.js';
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Inicializa o AOS
+  AOS.init({
+    duration: 1000, // Duração da animação em ms
+    once: true, // A animação acontece apenas uma vez
+    offset: 100, // Offset (em pixels) do ponto de ativação
+    easing: 'ease-in-out', // Função de easing
+  });
+
   carregarProjetos();
   const slide = new SlideNav('.slide', '.slide-wrapper');
   slide.init();
-  slide.addArrow('.prev', '.next')
+  slide.addArrow('.prev', '.next');
   slide.addControl('.custom-controls');
 });
